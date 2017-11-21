27c626e03d48d4623ad269e91e74cc22cb5a8609
Peter <pagrillet@gmail.com>
2017-11-21 11:50:52 +0000

    edit update, show and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,24 @@
 class ArticlesController < ApplicationController
+	def index
+		@articles = Article.all
+	end
+
 	def new 
 		@article = Article.new
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = "Your article has been updated"
+			redirect_to article_path(@article)
+		else
+			rneder 'edit'
+		end
 	end
 
 	def create
